b2bde35fb83012df8ac396c581761e61aeb270d8
German Bartoli <bartoligerman497@gmail.com>
2022-05-31 09:35:36 -0300

    Bitsion-Ficticia-S.A

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bitsion_Ficticia_S.A.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
